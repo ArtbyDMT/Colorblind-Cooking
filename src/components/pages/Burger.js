cdfa2efe218665997e858f2b40177f36dccf1866
ArtbyDMT <111263242+ArtbyDMT@users.noreply.github.com>
2022-11-04 15:15:50 -0500

    added images to burger timer

--- src/components/pages/Burger.js
+++ src/components/pages/Burger.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Burger.css';
 import FireTimer from '../firetimer/FireTimer';
+import burger from '../logo/burgerpatty.png';
 
 export default function Burger(){
     return(
         <div>
             <FireTimer 
+                image={burger}
             />
         </div>
     )
