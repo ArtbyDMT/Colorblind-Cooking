bba547aa00f448dc270a166fd1ff13dfd04c1b56
ArtbyDMT <111263242+ArtbyDMT@users.noreply.github.com>
2022-11-04 10:14:51 -0500

    Deleted inner divs that were unneded

--- src/components/pages/SteakOrBurger.js
+++ src/components/pages/SteakOrBurger.js
@@ -6,20 +6,19 @@ import FoodButton from "../buttons/FoodButton";
 export default function SteakOrBurger(){
     return(
         <div className="SteakOrBurger-container">
-            <div className="Steak">
+            
             <FoodButton className="steak"
                 type="submit"
                 href="/steak"
                 img={steak}
             />   
-            </div>
-            <div className="Burger">
+         
             <FoodButton className="burger"
                type="submit"
                href="/burger"
                img={burger}
             />
-            </div>
+          
         </div>
         )
 }
