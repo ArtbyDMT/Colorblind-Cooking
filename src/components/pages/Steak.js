3845412489d7c416ca898c6beb1a03519d81218c
ArtbyDMT <111263242+ArtbyDMT@users.noreply.github.com>
2022-11-04 15:16:19 -0500

    added steak image for timers

--- src/components/pages/Steak.js
+++ src/components/pages/Steak.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import FireTimer from '../firetimer/FireTimer';
 import './Steak.css';
+import steak from '../logo/steak2.png';
 
 export default function Steak() {
     return(
-        <FireTimer />
+        <FireTimer
+         image={steak}
+        />
     )
 }
