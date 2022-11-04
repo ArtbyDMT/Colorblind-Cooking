588082210de287a9bd9fa7561a4d5697b2a78b05
ArtbyDMT <111263242+ArtbyDMT@users.noreply.github.com>
2022-11-04 15:17:33 -0500

    added stuff to steakorburger

--- src/components/pages/SteakOrBurger.js
+++ src/components/pages/SteakOrBurger.js
@@ -1,24 +1,26 @@
 import React from "react";
 import burger from "../logo/burgerpatty.png"
-import steak from "../logo/steak.jpg"
+import steak from "../logo/steak2.png"
 import FoodButton from "../buttons/FoodButton";
+import './SteakOrBurger.css'
 
-export default function SteakOrBurger(){
+export default function SteakOrBurger(props){
+
     return(
         <div className="SteakOrBurger-container">
             
             <FoodButton 
-                className="steak"
                 type="submit"
                 href="/steak"
                 img={steak}
+                setSteakOrBurger={() => props.setSteakOrBurger("steak")}
             />   
          
             <FoodButton 
-               className="burger"
                type="submit"
                href="/burger"
                img={burger}
+               setSteakOrBurger={() => props.setSteakOrBurger("burger")}
             />
           
         </div>
