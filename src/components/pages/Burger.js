b3b7e050ccf25ad264d4e777cb20134be4d08edc
ArtbyDMT <111263242+ArtbyDMT@users.noreply.github.com>
2022-11-06 15:00:29 -0600

    added grill instructions

--- src/components/pages/Burger.js
+++ src/components/pages/Burger.js
@@ -9,6 +9,9 @@ export default function Burger(){
             <FireTimer 
                 image={burger}
             />
+            <h3>SET YOUR GRILL TO 350°-400°</h3>
+            <h3>ALLOW TO PREHEAT FOR 10-15 MINUTES</h3>
+            <h3>GET GRILLING</h3>
         </div>
     )
 
