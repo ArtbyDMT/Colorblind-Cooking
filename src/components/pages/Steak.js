d79c5f36057e2a49cbed4f7af8ab5ea240df0e5a
ArtbyDMT <111263242+ArtbyDMT@users.noreply.github.com>
2022-11-06 15:01:30 -0600

    added grill instructions

--- src/components/pages/Steak.js
+++ src/components/pages/Steak.js
@@ -5,8 +5,13 @@ import steak from '../logo/steak2.png';
 
 export default function Steak() {
     return(
+        <div className="steak-page">
         <FireTimer
          image={steak}
         />
+        <h3>SET YOUR GRILL TO 450°-500°</h3>
+            <h3>ALLOW TO PREHEAT FOR 10-15 MINUTES</h3>
+            <h3>GET GRILLING</h3>
+            </div>
     )
 }
