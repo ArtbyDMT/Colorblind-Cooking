86a49d86d9fa132870bc8b15a6d38dda98774bfa
ArtbyDMT <111263242+ArtbyDMT@users.noreply.github.com>
2022-11-06 15:02:09 -0600

    changed image to be background on css

--- src/components/pages/TimersPage.js
+++ src/components/pages/TimersPage.js
@@ -1,11 +1,9 @@
 import React from "react";
 import './TimersPage.css';
-import firegrill from "../logo/firegrill5.png";
 
 export default function TimersPage(){
     return(
         <div className="timerbody">
-            <img src={firegrill} alt=""/>
             </div>
     )
 }
